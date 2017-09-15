a1680c8f6e42bc6c2fa637355bbb372683bedbfb
Evgeny Rubanenko <erubanenko@gmail.com>
2017-09-16 00:16:06 +0300

    Add sequential realization

--- parallels/task_2/sequential.c
+++ parallels/task_2/sequential.c
@@ -5,41 +5,84 @@
 #include "runner.h"
 
 typedef struct ctx_t {
-  int *d;
-  int *s;
-  int a;
-  int b;
-  int x;
-  double p;
-  size_t N;
-  int P;
-  double boundary;
-  int res;
-  int steps;
-  double time;
+  int n;
+  int *data;
+  int *sorted;
+  double worktime;
 } ctx_t;
 
 void ctor(void *context) {
   size_t i;
-
   ctx_t *ctx;
+
   ctx = context;
 
-  ctx->d = calloc(ctx->N, sizeof(int));
-  assert(ctx->d);
+  ctx->data = calloc(ctx->n, sizeof(int));
+  assert(ctx->data);
 
-  ctx->s = calloc(ctx->N, sizeof(int));
-  assert(ctx->s);
+  ctx->sorted = calloc(ctx->n, sizeof(int));
+  assert(ctx->sorted);
 
-  for (i = 0; i < ctx->N; i++) {
-    ctx->d[i] = ctx->x;
+  for (i = 0; i < ctx->n; i++) {
+    ctx->data[i] = rand();
+    ctx->sorted[i] = ctx->data[i];
+  }
+}
+
+int cmpfunc(const void *a, const void *b) {
+  return (*(int *)a - *(int *)b);
+}
+
+void statistics(void *context) {
+  int i;
+  FILE *fp;
+  ctx_t *ctx;
+
+  ctx = context;
+
+  fp = fopen("seqstats.txt", "w+");
+
+  if (fp == NULL) {
+    fprintf(stderr, "FAILED TO PRINT STATISTICS\n");
+    exit(100500);
   }
 
-  for (i = 0; i < ctx->N; i++) {
-    ctx->s[i] = 0;
+  fprintf(fp, "%fs %d\n", ctx->worktime, ctx->n);
+
+  fp = fopen("seqdata.txt", "w+");
+
+  if (fp == NULL) {
+    fprintf(stderr, "FAILED TO PRINT DATA\n");
+    exit(100500);
   }
 
-  ctx->boundary = (1 - ctx->p) * ((double)RAND_MAX);
+  for (i = 0; i < ctx->n; ++i) {
+    fprintf(fp, "%d ", ctx->data[i]);
+  }
+
+  fprintf(fp, "\n\n\n\n\n");
+
+  for (i = 0; i < ctx->n; ++i) {
+    fprintf(fp, "%d ", ctx->sorted[i]);
+  }
+
+  fclose(fp);
+}
+
+void run(void *context) {
+  ctx_t *ctx;
+  struct timeval start, end;
+
+  ctx = context;
+
+  assert(gettimeofday(&start, NULL) == 0);
+
+  qsort(ctx->sorted, ctx->n, sizeof(int), cmpfunc);
+
+  assert(gettimeofday(&end, NULL) == 0);
+
+  ctx->worktime = ((end.tv_sec - start.tv_sec) * 1000000u + \
+                    end.tv_usec - start.tv_usec) / 1.e6;
 }
 
 void dtor(void *context) {
@@ -47,102 +90,22 @@ void dtor(void *context) {
 
   ctx = context;
 
-  free(ctx->d);
-  free(ctx->s);
-}
-
-int model(void *context, int i, int extra) {
-  int seed, decision;
-  double turn;
-  ctx_t *ctx;
-
-  seed = i + extra;
-  ctx = context;
-
-  while ((ctx->d[i] != ctx->a) && (ctx->d[i] != ctx->b)) {
-    turn = (double)rand_r(&seed);
-    if (turn > ctx->boundary) {
-      decision = 1;
-    } else {
-      decision = -1;
-    }
-    ctx->d[i] += decision;
-    ctx->s[i] += 1;
-  }
-
-  if (ctx->d[i] == ctx->b) {
-    return 1;
-  } else {
-    return 0;
-  }
-}
-
-void statistics(void *context) {
-  FILE *fp;
-  ctx_t *ctx;
-
-  ctx = context;
-
-  fp = fopen("stats1.txt", "w+");
-  fprintf(fp, "%.2f %.2f %.4fs %d %d %d %ld %.2f %d\n",
-          ((double) ctx->res) / ((double) ctx->N),
-          ((double) ctx->steps) / ((double) ctx->N),
-          ctx->time, ctx->a, ctx->b, ctx->x,
-          ctx->N, ctx->p, ctx->P);
-}
-
-void run(void *context) {
-  size_t i, j;
-  int sum, steps;
-  double worktime;
-  ctx_t *ctx;
-  struct timeval start, end;
-
-  sum = 0;
-  steps = 0;
-  ctx = context;
-
-  srand(time(NULL));
-  int extra = rand() % 1000;
-
-  assert(gettimeofday(&start, NULL) == 0);
-
-  for (i = 0; i < ctx->N; i++) {
-    sum += model(ctx, i, extra);
-    steps += ctx->s[i];
-  }
-
-  assert(gettimeofday(&end, NULL) == 0);
-
-  worktime = ((end.tv_sec - start.tv_sec) * 1000000u + \
-              end.tv_usec - start.tv_usec) / 1.e6;
-
-  ctx->res = sum;
-  ctx->steps = steps;
-  ctx->time = worktime;
-
   statistics(ctx);
+
+  free(ctx->data);
+  free(ctx->sorted);
 }
 
 int main(int argc, char **argv) {
-  int a, b, x, P;
-  double p;
-  size_t N;
+  int n;
 
-  a = atoi(argv[1]);
-  b = atoi(argv[2]);
-  x = atoi(argv[3]);
-  N = atoi(argv[4]);
-  p = atof(argv[5]);
+  n = atoi(argv[1]);
   
   ctx_t ctx = {
-      .a = a,
-      .b = b,
-      .x = x,
-      .p = p,
-      .N = N,
-      .P = P
+      .n = n
   };
+
+  srand(time(NULL));
 
   runner_pre(ctor, &ctx);
   
