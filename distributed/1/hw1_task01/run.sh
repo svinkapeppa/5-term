a3a90d20d3d715d10d9ac8022ca27abd45a8296a
Evgeny Rubanenko <erubanenko@gmail.com>
2017-11-16 23:48:56 +0300

    minor

--- distributed/1/hw1_task01/run.sh
+++ distributed/1/hw1_task01/run.sh
@@ -1,3 +1,3 @@
 #!/bash/bin/env bash
 
-curl -i "http://mipt-master.atp-fivt.org:50070/webhdfs/v1/data/wiki/en_articles_part?op=OPEN"
+curl -i "http://mipt-master.atp-fivt.org:50070/webhdfs/v1$1?op=OPEN"
---
+++ distributed/README.md
@@ -0,0 +1,7 @@
+# Big Data
+
+1:
+
+&emsp; Task is to write different scripts which give information about files in hdfs
+
+
