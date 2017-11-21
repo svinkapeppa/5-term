1dd32e4980a25661a23703b4956ab8c7ac3b1123
Evgeny Rubanenko <erubanenko@gmail.com>
2017-11-21 14:18:50 +0300

    proper 1 script

--- distributed/1/hw1_task01/run.sh
+++ distributed/1/hw1_task01/run.sh
@@ -1,3 +1,3 @@
-#!/bash/bin/env bash
+#! /usr/bin/env bash
 
-curl -i "http://mipt-master.atp-fivt.org:50070/webhdfs/v1$1?op=OPEN"
+curl -i "http://mipt-master.atp-fivt.org:50070/webhdfs/v1$1?op=OPEN" 2>/dev/null | python script.py
--- distributed/1/hw1_task01/script.py
+++ distributed/1/hw1_task01/script.py
@@ -3,12 +3,8 @@
 import re
 import sys
 
-reload(sys)
-sys.setdefaultencoding('utf-8')
-
 for line in sys.stdin:
-    words = line.split()
-    for word in words:
-	m = re.search('http://mipt-node\d+\.atp-fivt\.org', word)
-	if m is not None:
-	    print m.group(0)
+    m = re.match('Location: http://(.*):\d*/', line, flags=re.UNICODE)
+    if m is not None:
+        print m.group(1)
+        sys.exit()
