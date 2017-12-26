cae42f296158a4be6f2d2e3024dcd794cd7dde6f
Evgeny Rubanenko <erubanenko@gmail.com>
2017-12-26 21:31:25 +0300

    fix tabs

--- distributed/3/task_1/mapper.py
+++ distributed/3/task_1/mapper.py
@@ -4,7 +4,8 @@ import sys
 
 for line in sys.stdin:
     try:
-        id, text = line.split('\t')
+        id, text = line.split('\t', 1)
+        text = text.strip(' \t')
         print '%s\t%s\t%s' % (text[0], id, str(len(text)))
     except:
         continue
